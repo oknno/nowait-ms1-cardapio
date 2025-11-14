ab32f5f4fc4c7ccbf858ecde36eb150ec057ce22
fellipe0244 <fellipeandrade1000@gmail.com>
2025-11-14 20:12:18 -0300

    Remove pasta target do versionamento e adiciona .gitignore

--- src/test/java/backend/mesacerta/test/mesacertaTest.java
+++ src/test/java/backend/mesacerta/test/mesacertaTest.java
@@ -48,6 +48,7 @@ import backend.mesacerta.persistencia.mesacertaRepository;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {PropertyPlaceholderAutoConfiguration.class, mesacertaTest.DynamoDBConfig.class})
+@TestPropertySource(locations = "file:C:/Users/fellipe/Music/projeto/credenciais_cardapio/application.properties")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class mesacertaTest {
 
@@ -86,15 +87,15 @@ public class mesacertaTest {
 	@Test
 	public void teste1Criacao() throws ParseException {
 		LOGGER.info("Criando objetos...");
-		Cardapio c1 = new Cardapio("ETH_TESTE", "Ethereum", "Ethereum é um sistema de blockchain descentralizado e open-source, que possui sua própria criptomoeda, o Ether. O ETH funciona como uma plataforma para várias outras criptomoedas, bem como para a execução de smart contracts descentralizados.", 
+		Cardapio c1 = new Cardapio("SANDUBA", "Sanduiche", "Sanduiche de carne com tomate, alface, pao e queijo.", 
 				df.parse("30/06/2015"));
 		repository.save(c1);
 
-		Cardapio c2 = new Cardapio("ADA_TESTE", "Cardano", "Cardano é uma plataforma blockchain proof-of-stake que afirma ter o objetivo de permitir que “agentes de mudança, inovadores e visionários” tragam mudanças globais positivas.", 
+		Cardapio c2 = new Cardapio("MASSA", "Pizza", "Massa fina com calabresa e molho de tomate com borda recheada de catupiry.", 
 				df.parse("02/10/2017"));
 		repository.save(c2);
 
-		Cardapio c3 = new Cardapio("LINK_TESTE", "Chainlink", "Chainlink (LINK) é uma rede oracle descentralizada com o objetivo de conectar smart contracts com dados do mundo inteiro. A Chainlink foi desenvolvida por Sergey Nazarov, com Steve Ellis como cofundador. Foi realizado o ICO em setembro de 2017, arrecadando U$32 milhões, com o fornecimento total de 1 bilhão de tokens LINK.", 
+		Cardapio c3 = new Cardapio("REFRI", "Kuat", "Refrigerante gelado sabor guaraná.", 
 				df.parse("21/09/2017"));
 		repository.save(c3);
 		LOGGER.info("Pesquisado todos");
@@ -104,7 +105,7 @@ public class mesacertaTest {
 			LOGGER.info(cardapio.toString());
 		}
 		LOGGER.info("Pesquisado um objeto");
-		List<Cardapio> result = repository.findByNome("Cardano");
+		List<Cardapio> result = repository.findByNome("Pizza");
 		assertEquals(result.size(), 1);
 		LOGGER.info("Encontrado: {}", result.get(0).getCodigo());
 	}
@@ -119,7 +120,7 @@ public class mesacertaTest {
 			repository.delete(cardapio);
 			LOGGER.info("Apagando "+cardapio.toString());
 		}
-		List<Cardapio> result = repository.findByNome("Cardano");
+		List<Cardapio> result = repository.findByNome("Pizza");
 		assertEquals(result.size(), 0);
 		LOGGER.info("Exclusão feita com sucesso");
 		
